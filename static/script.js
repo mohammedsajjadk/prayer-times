b718732d98ee854c9de70ccdaba86f18a7ab9e11
Mohammed Sajjad Khatib <mskhatib@fexco.com>
2025-03-06 08:35:19 +0000

    UTC time fixed

--- static/script.js
+++
@@ -1,9 +0,0 @@
-// JavaScript to update time dynamically every second
-function updateTime() {
-    const timeElement = document.getElementById('time');
-    const currentTime = new Date().toLocaleTimeString();
-    timeElement.textContent = currentTime;
-}
-
-// Call updateTime every second to keep the time updated
-setInterval(updateTime, 1000);
