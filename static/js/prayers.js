574c9b2b6e0844e8b84dd38db4b49f33002b6f69
Mohammed Sajjad Khatib <mskhatib@fexco.com>
2025-10-25 08:48:21 +0100

    jumuah time reverted

--- static/js/prayers.js
+++ static/js/prayers.js
@@ -206,7 +206,7 @@ var prayerModule = {
     var jumuahTime;
     if (isIrishSummerTime) {
       // Summer Time
-      jumuahTime = "13:20";
+      jumuahTime = "13:45";
     } else {
       // Winter Time
       jumuahTime = "13:20";
